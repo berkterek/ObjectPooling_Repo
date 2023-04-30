0cbfe886812ffdde39026899e6e1680942f6c0ac
berkterek <42406263+berkterek@users.noreply.github.com>
2023-04-30 18:02:48 +0300

    feat: unity object pool system added

---
+++ ObjectPoolingTutorial/Assets/_Scripts/UnityPoolCarSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.Pool;
+
+public class UnityPoolCarSpawner : BaseCarSpawner
+{
+    IObjectPool<CarController> _pool1;
+
+    void Awake()
+    {
+        _pool1 = new ObjectPool<CarController>(() =>
+        {
+            var carObject =Instantiate(_carPrefab);
+            carObject.gameObject.SetActive(false);
+            carObject.transform.SetParent(this.transform);
+            return carObject;
+        },defaultCapacity:5);
+    }
+
+    public override void Despawn(CarController carController)
+    {
+        carController.gameObject.SetActive(false);
+        _pool1.Release(carController);
+    }
+
+    protected override void Spawn()
+    {
+        var poolObject = _pool1.Get();
+        poolObject.transform.position = GetRandomPosition();
+        poolObject.gameObject.SetActive(true);
+    }
+}
